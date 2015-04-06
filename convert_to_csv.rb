162dc7d81c4086882023b134ad3d14d93bc6a009
mconiaris <mconiaris@gmail.com>
2015-04-06 11:51:52 -0400

    Exploring options with docx gem.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -4,11 +4,17 @@ require 'pry'
 # Create a Docx::Document object for our existing docx file
 doc = Docx::Document.open('resources/questions.docx')
 
-binding.pry
+# binding.pry
 
 # Retrieve and display paragraphs
 doc.paragraphs.each do |p|
   puts p
 end
 
-
+# Retrieve and display paragraphs
+doc.paragraphs.each do |p|
+  if doc.to_s.include? "Rationale"
+    binding.pry
+    puts p
+  end
+end
