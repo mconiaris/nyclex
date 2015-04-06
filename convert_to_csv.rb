72a4d2dd16962c51eec19a90c83f0eb924048852
mconiaris <mconiaris@gmail.com>
2015-04-06 14:39:58 -0400

    Seperated Rationale: text from paragraphs.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -4,17 +4,19 @@ require 'pry'
 # Create a Docx::Document object for our existing docx file
 doc = Docx::Document.open('resources/questions.docx')
 
-# binding.pry
 
 # Retrieve and display paragraphs
 doc.paragraphs.each do |p|
   puts p
 end
 
+
 # Retrieve and display paragraphs
 doc.paragraphs.each do |p|
-  if doc.to_s.include? "Rationale"
-    binding.pry
-    puts p
-  end
+    # doc.paragraphs.first.text
+      # binding.pry
+    if p.text.include? "Rationale:"
+      puts p
+      puts "\n"
+    end
 end
