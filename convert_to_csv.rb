0caeb95d1903cf7a77932ba45f3b2dbbe9b1b1ea
mconiaris <mconiaris@gmail.com>
2015-04-06 11:09:14 -0400

    Added pry for testing.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -1,9 +1,14 @@
 require 'docx'
+require 'pry'
 
 # Create a Docx::Document object for our existing docx file
 doc = Docx::Document.open('resources/questions.docx')
+
+binding.pry
 
 # Retrieve and display paragraphs
 doc.paragraphs.each do |p|
   puts p
 end
+
+
