9f9bd07700b53cd1513684afcffdb8be1a81e02e
mconiaris <mconiaris@gmail.com>
2015-04-10 13:55:06 -0400

    Deleted redundant i variable and puts

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -4,7 +4,6 @@ require 'pry'
 # Create a Docx::Document object for our existing docx file
 doc = Docx::Document.open('resources/questions.docx')
 
-i = 0
 
 # Create or open stream to questions.txt document
 if File.exist?('resources/csvquestions.csv') == false
@@ -16,18 +15,14 @@ else
 end
 
 # binding.pry
-
 # Retrieve and display paragraphs and add to csvquestions
 # TODO: Use Regex to look for numbers for new row
 doc.paragraphs.each do |p|
-  i += 1
-
   # TODO: Replace newlines with commas
   if p.text != ""
     # Look for paragraphs that being with numbers
     if p.text.scan(/(\d+\.)/)[0] != nil
       q_file.print("\n")
-      puts "Numeric #{i}"
       puts p.text
     end
 
