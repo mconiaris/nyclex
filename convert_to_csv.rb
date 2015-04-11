4adc6b19d4b9242866286c6e454ae53a78d1e249
mconiaris <mconiaris@gmail.com>
2015-04-10 21:06:05 -0400

    Created functionality to write to questions_text file.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -11,7 +11,7 @@ questions_array = Array.new
 doc = Docx::Document.open('resources/questions.docx')
 
 
-# Create or open stream to questions.txt document
+# Create or open stream to csvquestions.csv document
 if File.exist?('resources/csvquestions.csv') == false
   q_file = File.new('resources/csvquestions.csv', 'w+')
   puts 'Created csvquestions.csv'
@@ -20,57 +20,57 @@ else
   puts 'Opened csvquestions.csv'
 end
 
+# Create or open stream to questions_text.txt document
+if File.exist?('resources/questions_text.txt') == false
+  qt_file = File.new('resources/questions_text.txt', 'w+')
+  puts 'Created questions_text.txt'
+else
+  qt_file = File.open('resources/questions_text.txt', 'w+')
+  puts 'Opened questions_text.txt'
+end
 
 # Retrieve and display paragraphs and add to csvquestions
 # TODO: Use Regex to look for numbers for new row
 doc.paragraphs.each do |p|
-#   # TODO: Replace newlines with commas
-#   if p.text != ""
-#     # Create Hash for each question
-#     # Look for paragraphs that being with numbers
-#     if p.text.scan(/(\d+\.)/)[0] != nil
-#       question_hash = Hash.new
-#       question_hash = { question: p.text }
-#       # q_file.print("\n")
-#     binding.pry
-#     else
-#       question_hash = { misc: p.text }
-#     end
-
-
-#     # q_file.print(p)
-#     q_file.print(p)
-#     # q_file.print(',')
-#   end
-
-  # Take Document Text and Insert it into an Array
-  if p.text != ""
-    text_line_array.push(p.text)
+  # Merge paragraphs into questions_text.txt document
+  if p.text != "" && p.text.scan(/(\d+\.)/)[0] != nil
+    qt_file.print("\n")
+    qt_file.print(p.text)
+    qt_file.print("\t")
+  elsif p.text != ""
+    qt_file.print(p.text)
   end
 end
 
-  i = 0
-# binding.pry
-  while i < text_line_array.size do
-    question = {
-      category:   text_line_array[0],
-      question:   text_line_array[i+1],
-      choice_A:   text_line_array[i+2],
-      choice_B:   text_line_array[i+3],
-      choice_C:   text_line_array[i+4],
-      choice_D:   text_line_array[i+5],
-      answer:     text_line_array[i+6],
-      iggy:       text_line_array[i+7],
-      rationale:  text_line_array[i+8],
-      subject:    text_line_array[i+9]
-    }
+puts qt_file.size
 
-    questions_array.push(question)
-    puts text_line_array[i]
-    i += 9
-  end
+while(line = qt_file.gets)
+  puts "testing"
+  puts(line)
+end
 
-    binding.pry
+
+#   i = 0
+#   while i < text_line_array.size do
+#     question = {
+#       category:   text_line_array[0],
+#       question:   text_line_array[i+1],
+#       choice_A:   text_line_array[i+2],
+#       choice_B:   text_line_array[i+3],
+#       choice_C:   text_line_array[i+4],
+#       choice_D:   text_line_array[i+5],
+#       answer:     text_line_array[i+6],
+#       iggy:       text_line_array[i+7],
+#       rationale:  text_line_array[i+8],
+#       subject:    text_line_array[i+9]
+#     }
+
+#     questions_array.push(question)
+#     puts text_line_array[i]
+#     i += 9
+#   end
+
+    # binding.pry
 
 # # Retrieve and display paragraphs
 # doc.paragraphs.each do |p|
@@ -89,3 +89,10 @@ q_file.puts("Testing")
 puts q_file.closed?
 q_file.close
 puts q_file.closed?
+
+puts qt_file.closed?
+qt_file.close
+puts qt_file.closed?
+
+# REGEX:
+# (\d+\.)(.+)([A]\.)(.+)([B]\.)(.+)([C]\.)(.+)([D]\.)(.+)(Ans:|ANS:)(.[A-F]|[A-F])(Iggy:|.Iggy|Iggy)(.+)(Rationale:)(.+)(Health.+|Physiological.+)
