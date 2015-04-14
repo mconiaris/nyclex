622c51a0d400c6c6534450b085fe75136b249796
mconiaris <mconiaris@gmail.com>
2015-04-14 15:29:37 -0400

    Another subject added in if statement for line break.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -44,7 +44,8 @@ def process_word_document
       p.text == "Physiological Adaptation, Application, Assessment" ||
       p.text == "CARDIOVASCULAR/CIRCULATORY SYSTEM (INCLUDING OPERATIVE)" ||
       p.text == "Pharmacology, Application, Evaluation (Teaching)" ||
-      p.text == "Reduction of Risk, Application, Assessment"
+      p.text == "Reduction of Risk, Application, Assessment" ||
+      p.text == "Physiological Adaptation, Application, Intervention"
         qt_file.puts(p.text)
      elsif p.text == "GASTROINTESTINAL SYSTEM"
         qt_file.print("\n")
