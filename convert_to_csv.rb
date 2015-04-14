0a4bc1b4203cd8b8d41a08d713d8c5a81de14286
mconiaris <mconiaris@gmail.com>
2015-04-14 15:43:28 -0400

    Blanked out redundant or statement in if block for newlines.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -45,8 +45,9 @@ def process_word_document
       p.text == "CARDIOVASCULAR/CIRCULATORY SYSTEM (INCLUDING OPERATIVE)" ||
       p.text == "Pharmacology, Application, Evaluation (Teaching)" ||
       p.text == "Reduction of Risk, Application, Assessment" ||
-      p.text == "Physiological Adaptation, Application, Intervention" ||
-      p.text == "Pharmacology, Application, Planning"
+      # p.text == "Physiological Adaptation, Application, Intervention" ||
+      p.text == "Pharmacology, Application, Planning" ||
+      p.text.include?("Physiological Adaptation, Application, Intervention")
         qt_file.puts(p.text)
      elsif p.text == "GASTROINTESTINAL SYSTEM"
         qt_file.print("\n")
