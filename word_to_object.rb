0e8116549ec5c5492edbca550d949d115b951e06
mconiaris <mconiaris@gmail.com>
2015-04-17 07:27:58 -0400

    Added requires. Opened Word document.

--- word_to_object.rb
+++ word_to_object.rb
@@ -1,8 +1,15 @@
+require_relative 'question'
+require 'docx'
+require 'pry'
+
 class WordToObject
 
   def word_to_array
 
+    # Create a Docx::Document object for our existing docx file
+    doc = Docx::Document.open('resources/questions.docx')
 
+  # binding.pry
 
   end
 
