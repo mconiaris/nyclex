e320cce64cf4ec1fc183dae49f641843bd087743
mconiaris <mconiaris@gmail.com>
2015-04-17 09:07:49 -0400

    Added return line for doc_line_array in word_doc_to_array method

--- word_to_object.rb
+++ word_to_object.rb
@@ -3,12 +3,10 @@ require 'docx'
 require 'pry'
 
 
-
-
 def word_doc_to_array
+
   # Create Array of Document Lines
   doc_line_array = Array.new
-
 # question_number, :question_text,
 #     :choice_a, :choice_b, :choice_c, :choice_d,
 #     :choice_e, :choice_f, :correct_answer, :iggy,
@@ -24,7 +22,10 @@ def word_doc_to_array
       doc_line_array.push(p.to_s)
     end
   end
-  binding.pry
+  return doc_line_array
 end
 
 word_doc_to_array
+puts word_doc_to_array.size
+# binding.pry
+puts "hi"
