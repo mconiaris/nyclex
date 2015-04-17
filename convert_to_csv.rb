a35085c8f8f21b6abc695c0c435a78ae41641b9d
mconiaris <mconiaris@gmail.com>
2015-04-17 15:37:07 -0400

    Changed convert_to_csv print method to txt file and added QUESTION and QBREAK text for splitting.

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -32,34 +32,47 @@ def process_word_document
     puts 'Opened questions_text.txt'
   end
 
+  qt_file.print("QUESTION")
+
   # Retrieve and display paragraphs and add to questions_text.txt
   doc.paragraphs.each do |p|
     # Merge paragraphs into questions_text.txt document
     # Each newline contains a question and related text
     # Search for question numbers in first six characters
-    if p.text != "" && p.text[0...5].scan(/(\d+\.)/)[0] != nil
-      qt_file.print("\n")
-      qt_file.print(p.text)
-      qt_file.print(" ")
-    # Add line break after subjects to capture questions
-    # that do not have line numbers
-    elsif p.text == "Health Promotion and Maintenance, Application, Assessment" ||
-      p.text == "Physiological Adaptation, Application, Assessment" ||
-      p.text == "CARDIOVASCULAR/CIRCULATORY SYSTEM (INCLUDING OPERATIVE)" ||
-      p.text == "Pharmacology, Application, Evaluation (Teaching)" ||
-      p.text == "Reduction of Risk, Application, Assessment" ||
-      p.text == "Pharmacology, Application, Planning" ||
-      p.text.include?("Physiological Adaptation, Application, Intervention")
-        qt_file.puts(p.text)
-      # Add line break for this category
-     elsif p.text == "GASTROINTESTINAL SYSTEM"
-        qt_file.print("\n")
-        qt_file.puts(p.text)
+    if p.to_s.include?("CARDIOVASCULAR\/CIRCULATORY SYSTEM") ||
+      p.to_s.include?("Reduction of risk") ||
+      p.to_s.include?("Health Promotion and Maintenance") ||
+      p.to_s.include?("Physiological Adaptation, Assessment, Application") ||
+      p.to_s.include?("Basic Care and Comfort, Knowledge") ||
+      p.to_s.include?("Physiological Adaptation, Application") ||
+      p.to_s.include?("Risk Reduction, Application,") ||
+      p.to_s.include?("Reduction of Risk, Application, Implementation") ||
+      p.to_s.include?("Reduction of Risk") ||
+      p.to_s.include?("Pharmacology, Application, Analysis") ||
+      p.to_s.include?("Pharmacology, Application, Implementation") ||
+      p.to_s.include?("Physiological Integrity, Analysis,") ||
+      p.to_s.include?("Safe and Effective Care, Implementation,") ||
+      p.to_s.include?("GASTROINTESTINAL SYSTEM") ||
+      p.to_s.include?("Pharmacology, Application, Evaluation") ||
+      p.to_s.include?("Physiological Integrity, Application, Implementation") ||
+      p.to_s.include?("Physiological adaptation, Application,") ||
+      p.to_s.include?("Physiological adaptation, analysis, implementation") ||
+      p.to_s.include?("Basic Care and Comfort, knowledge, evaluation") ||
+      p.to_s.include?("Physiological adaptation, application,") ||
+      p.to_s.include?("Pharmacology, Application, Planning") ||
+      p.to_s.include?("Management of Care, Application, Assessment") ||
+      p.to_s.include?("Physiological Adaptation, Knowledge") ||
+      p.to_s.include?("Safe and effective care, Knowledge,") ||
+      p.to_s.include?("Physiological integrity, analysis, assessment") ||
+      p.to_s.include?("Physiological integrity, Application, Implementation")
+
+    qt_file.puts(p.text)
+    qt_file.print("QUESTION")
     # Print questions without questions numbers to document
     elsif p.text != ""
-      binding.pry
+      # binding.pry
       qt_file.print(p.text)
-      qt_file.print(" ")
+      qt_file.print("QBREAK")
     end
   end
   # Add test text to ensure that file can be written to
@@ -129,7 +142,7 @@ def process_text_file
     end
     puts paragraph
   end
-    binding.pry
+    # binding.pry
   text_file_text = text_file.read
 
   # Look for paragraphs that fit into working REGEX
