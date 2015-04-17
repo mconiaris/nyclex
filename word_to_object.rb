d90551802e6fa3d0bd586ae04c444e54ae2bd6af
mconiaris <mconiaris@gmail.com>
2015-04-17 08:42:38 -0400

    Import Word Document text into an array. Delete leading whitespace.

--- word_to_object.rb
+++ word_to_object.rb
@@ -2,19 +2,29 @@ require_relative 'question'
 require 'docx'
 require 'pry'
 
+
+
+
 def word_doc_to_array
+  # Create Array of Document Lines
+  doc_line_array = Array.new
 
-  # # Create a Docx::Document object for our existing docx file
-  # doc = Docx::Document.open('resources/questions.docx')
+# question_number, :question_text,
+#     :choice_a, :choice_b, :choice_c, :choice_d,
+#     :choice_e, :choice_f, :correct_answer, :iggy,
+#     :rationale, :subject, :category
 
-  # # Retrieve and display paragraphs and add to questions_text.txt
-  # doc.paragraphs.each do |p|
-  # binding.pry
+  # Create a Docx::Document object for our existing docx file
+  doc = Docx::Document.open('resources/questions.docx')
 
-  #   puts p
+  # Retrieve and display paragraphs and add to questions_text.txt
+  doc.paragraphs.each do |p|
 
-  # end
-  puts "test"
+    if p.to_s != ""
+      doc_line_array.push(p.to_s)
+    end
+  end
+  binding.pry
 end
 
 word_doc_to_array
