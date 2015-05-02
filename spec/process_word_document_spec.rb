67c8130988d063dfbdb8fd669f0252902d32f05e
mconiaris <mconiaris@gmail.com>
2015-05-02 10:28:19 -0400

    Changed process_word_document_spec.rb to make it faster. Removed unnessary second execution to the process_word_document method.

--- spec/process_word_document_spec.rb
+++ spec/process_word_document_spec.rb
@@ -3,22 +3,6 @@ require_relative "../lib/process_word_document"
 
 RSpec.describe "ProcessWordDocument" do
 
-
-  context "Meta Data after #process_word_document" do
-    before(:context) do
-      process_word_document
-      @text = File.stat('resources/questions_text.txt')
-    end
-
-    it "questions_text.txt has been created" do
-      expect(File.exists?("resources/questions_text.txt")).to eq(true)
-    end
-
-    it "the returned file size is 38844" do
-      expect(@text.size).to eq(38844)
-    end
-  end
-
   context "File data after #process_word_document" do
     before(:context) do
       process_word_document
@@ -31,6 +15,20 @@ RSpec.describe "ProcessWordDocument" do
 
     it "questions_text.txt is opened" do
       expect(@text.closed?).to eq(false)
+    end
+  end
+
+  context "Meta Data after #process_word_document" do
+    before(:context) do
+      @text = File.stat('resources/questions_text.txt')
+    end
+
+    it "questions_text.txt has been created" do
+      expect(File.exists?("resources/questions_text.txt")).to eq(true)
+    end
+
+    it "the returned file size is 38844" do
+      expect(@text.size).to eq(38844)
     end
   end
 end
