36482db3370c23fdb12ed54b0e26988ae3db3209
mconiaris <mconiaris@gmail.com>
2015-05-02 10:24:47 -0400

    Revised language in app_spec.rb and added two respond_to tests.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,70 +8,75 @@ RSpec.describe "#process_text_file" do
     @text = process_text_file
   end
 
-  context "the questions file" do
-    it "returns an array" do
-      expect(@text.class).to eq(Array)
+  context "the questions file creates an object that" do
+    it "responds to the #each method" do
+      expect(@text).to respond_to(:each)
       # binding.pry
     end
 
-    it "the returned array size is 54" do
+    it "responds to the #size method" do
+      expect(@text).to respond_to(:size)
+      # binding.pry
+    end
+
+    it "has a returned size of 54" do
       expect(@text.size).to eq(54)
     end
 
-    it "array's first cell contains text" do
+    it "has a first cell that contains text" do
       expect(@text[0].class).to eq(String)
     end
 
-    it "array's first cell includes 'CARDIO'" do
+    it "has a first cell that includes 'CARDIO'" do
       expect(@text[0]).to include("CARDIO")
     end
 
-    it "array's second cell includes 'QBREAK'" do
+    it "has a second cell that includes 'QBREAK'" do
       expect(@text[1]).to include("QBREAK")
     end
 
-    it "array's cell 5 begins with '5.'" do
+    it "begins with '5.' in cell 5" do
       expect(@text[5][0...2]).to eq("5.")
     end
 
-    it "array's cell 10 begins with '11.'" do
+    it "begins with '11.' in cell 10" do
       expect(@text[10][0...3]).to eq("11.")
     end
 
-    it "array's cell 15 begins with '16.'" do
+    it "begins with '16.' in cell 15" do
       expect(@text[15][0...3]).to eq("16.")
     end
 
-    it "array's cell 20 begins with '21.'" do
+    it "begins with '21.' in cell 20" do
       expect(@text[20][0...3]).to eq("21.")
     end
 
-    it "array's cell 24 includes 'GASTRO'" do
+    it "includes 'GASTRO' in cell 24" do
       expect(@text[24]).to include("GASTRO")
     end
 
-    it "array's cell 30 begins with 'A c'" do
+    it "begins with 'A c' in cell 30" do
       expect(@text[30][0...3]).to eq("A c")
     end
 
-    it "array's cell 35 begins with 'The'" do
+    it "begins with 'The' in cell 35" do
       expect(@text[35][0...3]).to eq("The")
     end
 
-    it "array's cell 40 begins with '38.'" do
+    it "begins with '38.' in cell 40" do
       expect(@text[40][0...3]).to eq("38.")
     end
 
-    it "array's cell 45 begins with '43.'" do
+    it "begins with '43.' in cell 45" do
       expect(@text[45][0...3]).to eq("43.")
     end
 
-    it "array's cell 50 begins with '48.'" do
+    it "begins with '48.' in cell 50" do
       expect(@text[50][0...3]).to eq("48.")
     end
 
     # binding.pry
-    it "array's cell 53 begins with '51.'" do
+    it "begins with '51.' in cell 53" do
       expect(@text[53][0...3]).to eq("51.")
     end
 
