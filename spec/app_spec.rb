555e289cae5ec9ce32db6c8747e6c3255d24c935
mconiaris <mconiaris@gmail.com>
2015-05-04 17:05:41 -0400

    Added tests for cells 50 & 51

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -139,8 +139,8 @@ RSpec.describe "#normalize_object_text" do
     @objects_array = turn_text_into_objects(@text_array)
     normalize_object_text(@objects_array)
   end
-  context "the container of paragraphs" do
-    it "cell 0 returns a Question Object" do
+  context "the container" do
+    it "returns a Question Object from cell 0" do
       expect(@objects_array[0].class).to eq(Question)
     end
   end
@@ -148,20 +148,16 @@ RSpec.describe "#normalize_object_text" do
     it "cell 1's @question_text beigins with 'When'" do
       expect(@objects_array[1].question_text[0...4]).to eq('When')
     end
+    it "cell 50's @question_text beigins with 'The nu'" do
+      expect(@objects_array[50].question_text[0...6]).to eq('The nu')
+    end
+    it "cell 51's @question_text beigins with 'Select'" do
+      expect(@objects_array[51].choice_a[0...6]).to eq('Select')
+    end
   end
 end
 
 
-# # Tests that Data from Text Document Turns Into Question Objects
-# RSpec.describe "#turn_text_into_objects" do
-#   before(:context) do
-#     @text_array = process_text_file
-#     @objects_array = turn_text_into_objects(@text_array)
-#   end
-#   context "the container of paragraphs" do
-#     it "cell 0 returns a Question Object" do
-#       expect(@objects_array[0].class).to eq(Question)
-#     end
 #
 #     it "cell 5's @choice_a beigins with 'A drop'" do
 #       expect(@objects_array[5].choice_a[0...6]).to eq('A drop')
@@ -190,11 +186,6 @@ end
 #     it "cell 45's @choice_e beigins with 'Gatro'" do
 #       expect(@objects_array[45].category[0...6]).to eq('Gastro')
 #     end
-#     it "cell 50's @question_text beigins with 'The nu'" do
-#       expect(@objects_array[50].question_text[0...6]).to eq('The nu')
-#     end
-#     it "cell 51's @question_text beigins with 'Select'" do
-#       expect(@objects_array[51].choice_a[0...6]).to eq('Select')
-#     end
+#
 #   end
 # end
