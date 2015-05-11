cac9c43026a7060b4d37778fc09fffc14709abe6
mconiaris <mconiaris@gmail.com>
2015-05-11 08:31:53 -0400

    Created first test for App

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,4 +3,15 @@ require 'json'
 require_relative "../lib/app"
 
 
-
+RSpec.describe "App", :app do
+  before(:context) do
+    @text_file = process_word_document
+    @text_file_array = process_text_file
+    @question_objects_array = turn_text_into_objects(@text_file_array)
+    normalize_object_text(@question_objects_array)
+    persist_objects(@question_objects_array)
+  end
+  it "responds to the each method" do
+    expect(@question_objects_array).to respond_to(:each)
+  end
+end
