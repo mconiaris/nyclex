0a0e9a6e6e85e6c7501accf22e66dafbbe4dd63a
mconiaris <mconiaris@gmail.com>
2015-05-11 09:09:33 -0400

    Revised test for ProcessWordDocument to confirm that it generates a readable file

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,13 +5,17 @@ require_relative "../lib/app"
 
 RSpec.describe "App", :app do
   before(:context) do
-    @text_file = process_word_document
+    process_word_document
     @text_file_array = process_text_file
     @question_objects_array = turn_text_into_objects(@text_file_array)
     normalize_object_text(@question_objects_array)
     persist_objects(@question_objects_array)
   end
-  it "responds to the each method" do
-    expect(@question_objects_array).to respond_to(:each)
+  describe "ProcessWordDocument" do
+    it "returns a readable file" do
+      @text_file = File.stat('resources/questions_text.txt')
+      # binding.pry
+      expect(@text_file.readable?).to eq(true)
+    end
   end
 end
